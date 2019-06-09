deefcdfad522d96542e8cef1df7e0acbe0b01c66
RohanRathodRajput <rohanrathodrajput@gmail.com>
2019-06-09 13:20:45 +0530

    Added println messages

--- GitProject/src/GitPackage/GitClass.java
+++ GitProject/src/GitPackage/GitClass.java
@@ -3,8 +3,8 @@ package GitPackage;
 public class GitClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hey Gitty!!!");
-		System.out.println()
+		System.out.println("Hey Jenny!!!");
+		System.out.println("Hello Gitty!!!");
 	}
 
 }
